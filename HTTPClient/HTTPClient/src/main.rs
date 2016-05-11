dd9436007caff0d4d127882f2b49438cdaf017f4
urcra <urcra@urcra.com>
2016-05-11 05:16:29 +0100

    Working on seperating function, printing the body is now also possible

--- HTTPClient/HTTPClient/src/main.rs
+++ HTTPClient/HTTPClient/src/main.rs
@@ -4,7 +4,84 @@ use std::io::prelude::*;
 use std::env::args;
 use std::process;
 use std::net::TcpStream;
+use std::collections::HashMap;
 use regex::Regex;
+
+
+
+struct HttpHeaders {
+    headers: HashMap<String, String>
+}
+
+impl HttpHeaders {
+    fn add(&mut self, header: &str) {
+        let split: Vec<&str> = header.split(':').collect();
+        let tag = split[0].trim().to_string();
+        let value = split[1].trim().to_string();
+
+        self.headers.insert(tag, value);
+    }
+
+    fn get(&self, tag: &str) -> &str{
+        self.headers.get(tag).unwrap()
+    }
+
+    fn to_string(&self) -> String{
+        let mut acum = String::new();
+        for (tag, value) in &self.headers {
+            acum = acum + &tag + " : " + &value + "\r\n";
+        }
+        acum
+    }
+
+    fn new() -> HttpHeaders {
+        HttpHeaders{headers: HashMap::new()}
+    }
+}
+
+
+struct HttpMessage {
+    init_line: String,
+    headers: HttpHeaders,
+    body: String,
+}
+
+impl HttpMessage {
+    fn to_string(&self) -> String {
+        String::new() + &self.init_line + "\r\n" + &self.headers.to_string() + "\r\n" + &self.body
+    }
+
+    fn from_str(message: &str) -> HttpMessage {
+        let mut lines = message.lines();
+        let init_line = (&lines.next().unwrap()).to_string();
+        let mut headers = HttpHeaders::new();
+
+        // TODO make this better
+
+        loop {
+            let line = lines.next();
+            match line {
+                Some("") => break,
+                Some(x) => headers.add(x),
+                None    => break,
+            };
+        }
+
+        let mut body = String::new();
+
+        loop {
+            let line = lines.next();
+            match line {
+                Some(x) => body = body + x + "\r\n",
+                None    => break,
+            };
+        }
+
+
+        HttpMessage {init_line: init_line, headers: headers, body: body}
+    }
+}
+
 
 
 fn main() {
@@ -54,7 +131,11 @@ fn main() {
 
     let _ = stream.read_to_string(&mut response);
 
+    let body = HttpMessage::from_str(&response).body;
+
     println!("Response from server was {:?}", response);
+
+    println!("body is {}", body);
 
 
 }
