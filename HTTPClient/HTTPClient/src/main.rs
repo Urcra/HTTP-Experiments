d1fc217ade1029209866fe4dec3cfe8f57deb47e
urcra <urcra@urcra.com>
2016-05-17 05:58:08 +0100

    Complete rework of code. Should be cleaner to look at now
    Still missing handling of 3xx codes

--- HTTPClient/HTTPClient/src/main.rs
+++ HTTPClient/HTTPClient/src/main.rs
@@ -1,173 +1,136 @@
 extern crate regex;
 
 use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufWriter;
 use std::env::args;
 use std::process;
-use std::fs::File;
-use std::net::TcpStream;
+use std::str;
 use std::collections::HashMap;
+use std::net::TcpStream;
 use regex::Regex;
-
-
-struct HttpHeaders {
-    headers: HashMap<String, String>
-}
-
-impl HttpHeaders {
-    fn add_str(&mut self, header: &str) {
-        let split: Vec<&str> = header.splitn(2, ':').collect();
-        let tag = split[0].trim().to_string();
-        let value = split[1].trim().to_string();
-
-        self.headers.insert(tag, value);
-    }
-
-    fn add(&mut self, tag: &str, value: &str) {
-        self.headers.insert(tag.to_string(), value.to_string());
-    }
-
-    fn get(&self, tag: &str) -> &str{
-        self.headers.get(tag).unwrap()
-    }
-
-    fn to_string(&self) -> String{
-        let mut acum = String::new();
-        for (tag, value) in &self.headers {
-            acum = acum + &tag + ": " + &value + "\r\n";
-        }
-        acum
-    }
-
-    fn new() -> HttpHeaders {
-        HttpHeaders{headers: HashMap::new()}
-    }
-}
-
-
-struct HttpMessage {
-    init_line: String,
-    headers: HttpHeaders,
-    body: String,
-}
-
-impl HttpMessage {
-    fn to_string(&self) -> String {
-        String::new() + &self.init_line + "\r\n" + &self.headers.to_string() + "\r\n" + &self.body
-    }
-
-    fn from_str(message: &str) -> HttpMessage {
-        let splitted: Vec<&str> = message.splitn(2, "\r\n\r\n").collect();
-
-        let mut headerlines = splitted[0].lines();
-        let body = splitted[1].to_string();
-
-        let init_line = (&headerlines.next().unwrap()).to_string();
-
-        let mut headers = HttpHeaders::new();
-        for line in headerlines {
-            headers.add_str(line);
-        }
-
-        HttpMessage {init_line: init_line, headers: headers, body: body}
-    }
-
-    // Dpn't use this if the message is not a response
-    fn status_code(&self) -> &str{
-        self.init_line.split_whitespace().collect::<Vec<_>>()[1]
-    }
-
-    fn new() -> HttpMessage {
-        HttpMessage {init_line: String::new(), headers: HttpHeaders::new(), body: String::new()}
-    }
-}
 
 
 
 fn main() {
 
-    let url;
-    let dest;
 
-    let args: Vec<String> = args().collect();
+    let url = args().nth(1).expect("Usage:\n\t tool <url> [/path/to/file]");
 
-    if args.len() == 3 {
-        url = args[1].clone();
-        dest = args[2].clone();
-    } else {
-        println!("Usage:\n\t tool <url> </path/to/file>");
-        process::exit(0);
-    }
+    let mut output: BufWriter<Box<Write>> = BufWriter::new(
+        if let Some(f) = args().nth(2) {
+            Box::new(File::create(f).expect("Could not create file"))
+        } else {
+            Box::new(std::io::stdout())
+        });
+
+
 
     let (host, file) = parse_uri(&url);
 
-    let mut response = String::new();
+    let stream = send_request(host, file);
 
-    let mut connhost = host.to_string();
-
-    let mut request = HttpMessage::new();
-    let mut hresponse = HttpMessage::new();
-
-    request.init_line = format!("GET {} HTTP/1.1", file);
-    request.headers.add("Host", host);
-    request.headers.add("Connection", "close");
-
-    loop {
-        let mut stream = TcpStream::connect(&format!("{}:80", &connhost) as &str).unwrap();
-        stream.write(request.to_string().as_bytes());
-        stream.read_to_string(&mut response);
-        hresponse = HttpMessage::from_str(&response);
+    let (init_line, mut headers, stream) = parse_response_head(stream);
 
 
-        let status_code = hresponse.status_code();
-        println!("Handling status code {}", status_code);
+    let mut buffer = String::new();
+    let mut reader = BufReader::new(&stream);
 
-        if status_code == "301" {
-            let url = hresponse.headers.get("Location");
-            let (host, file) = parse_uri(url);
-            request.init_line = format!("GET {} HTTP/1.1", file);
-            request.headers.add("Host", host);
-            connhost = host.to_string();
-        }else if status_code == "200" {
-            break;
-        } else {
-            println!("Unhandled status code {}", status_code);
-            break;
+
+
+    if let Some(encoding) = headers.get("Transfer-Encoding") {
+        if encoding == "chunked" {
+            loop {
+                reader.read_line(&mut buffer);
+                println!("{:?}", buffer);
+                buffer = buffer.trim().to_string();
+                if buffer == "0"{
+                    buffer.clear();
+                    break;
+                }
+                let content_length: u64 = u64::from_str_radix(&buffer, 16).unwrap();
+
+                let mut chunk = (&mut reader).take(content_length + 2);
+                let mut outchunk: Vec<u8> = Vec::new(); 
+                chunk.read_to_end(&mut outchunk);
+                output.write_all(&outchunk[0..outchunk.len()-2]);
+                //chunk.read_to_end(&mut body);
+
+                buffer.clear();
+            }
         }
+    } else {
+        let length = headers.get("Content-Length").unwrap();
+        let content_length: u64 = u64::from_str_radix(&length, 10).unwrap();
 
-        response = String::new();
+        let mut chunk = (&mut reader).take(content_length);
+        let mut outchunk: Vec<u8> = Vec::new(); 
+        chunk.read_to_end(&mut outchunk);
+        output.write_all(&outchunk);
     }
 
 
-    let body = HttpMessage::from_str(&response).body;
+    println!("Init line was  {:?}", init_line);
+    println!("Headers were {:?}", headers);
 
-    //println!("Response from server was {:?}", response);
 
-    //println!("body is {}", body);
 
-    let mut body_file = match File::create(&dest) {
-        Err(_)      => panic!("Coudln't create file"),
-        Ok(file)    => file,
-    };
+}
 
-    match body_file.write_all(body.as_bytes()) {
-        Err(_)      => panic!("Coudln't write to file"),
-        Ok(_)       => println!("Copied contents from {} into {}", url, dest),
-    };
+fn send_request<'a>(host: &str, file: &str) -> TcpStream {
+    let mut stream = TcpStream::connect(&format!("{}:80", host) as &str).unwrap();
+
+    let message = format!("GET {} HTTP/1.1\r\n\
+                        Host: {}\r\n\
+                        Connection: close\r\n\
+                        User-Agent: SillyGoose/0.1\r\n\
+                        \r\n", file, host);
+
+    let _ = stream.write(message.as_bytes());
+
+    stream
+}
+
+fn parse_response_head(stream: TcpStream) -> (String, HashMap<String, String>, TcpStream) {
+    let mut buffer = String::new();
+    let mut init_line = String::new();
+    let mut headers = HashMap::new();
+    // More fun lifetimes hacks
+    {
+        let mut reader = BufReader::new(&stream);
+
+        let _ = reader.read_line(&mut init_line);
+
+        loop {
+            reader.read_line(&mut buffer);
+            if buffer == "\r\n" {
+                buffer.clear();
+                break;
+            } else {
+                // Not an unnecessary scope, needed for the borrow checker to not complain
+                {
+                    let split: Vec<&str> = buffer.splitn(2, ':').collect();
+                    let tag = split[0].trim().to_string();
+                    let value = split[1].trim().to_string();
+
+                    headers.insert(tag, value);
+                }
+                buffer.clear();
+            }
+        }
+    }
+    (init_line, headers, stream)
 }
 
 fn parse_uri(uri: &str) -> (&str, &str) {
-
     // Url regex taken from RFC3986 https://tools.ietf.org/html/rfc3986#appendix-B
     let url_regex = Regex::new(r"^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?").unwrap();
 
     let captured = url_regex.captures(uri).unwrap();
 
-    println!("{}", uri);
-
-    let host = captured.at(4).expect("Malformed url").trim();
-    let file = captured.at(5).expect("Malformed url").trim();
-
-    println!("host {} file {}", host, file);
+    let host = captured.at(4).expect("Malformed url");
+    let file = captured.at(5).expect("Malformed url");
 
     (host, file)
 }
